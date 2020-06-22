626656a14e20da9a3e77ad46765dfbd50ca34bd5
Ömer Ayyıldız <oayyildiz416@gmail.com>
2020-06-22 14:20:37 +0300

    Fixed mini bug

--- setup.sh
+++ setup.sh
@@ -9,9 +9,10 @@ reset=`tput sgr0`
 line="--------------------------------------"
 cd sources
 #for gui-settings. Because the gnome settings can not set another distros from main distro.
-echo -e "1.Pardus\n2.Ubuntu\"
+echo -e "1.Pardus\n2.Ubuntu"
 echo $line
 read -p "${yellow}Distronunuzu Seçin / Choose Your Distro:${reset}" gnomeSet
+   
    if [ $gnomeSet == 2 ]; then
      cd gui-settings
      rm -f dconf-settings.ini 
